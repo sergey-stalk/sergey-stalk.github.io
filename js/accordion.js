fe6194e41d6957f1c886660e8c07ffd8b726f389
sergey-stalk <s_matveev95@mail.ru>
2017-11-06 15:16:16 +0200

    del

--- js/accordion.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-  $(".services p").hide().prev().click(function() {
-  $(".services p").slideUp().removeClass().prev('h3').removeClass();
-  $(this).next().not(":visible").slideDown().addClass("active").prev('h3').addClass("active");
-  });
-});
